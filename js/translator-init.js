719d5325dc9e230863901f5d3e84e1e5ef581509
Apo <apolline.oresic@gmail.com>
2026-04-03 10:49:23 +0200

    Update translator-init.js

--- js/translator-init.js
+++ js/translator-init.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
 
+    // boutons langues
+    document.querySelectorAll(".lang-btn").forEach(btn => {
+        btn.addEventListener("click", function() {
+          const lang = this.getAttribute("data-lang");
+
+           // Appel ConveyThis
+            if (typeof ConveyThis !== "undefined") {
+                ConveyThis.switchLanguage(lang);
+            } else {
+                console.error("ConveyThis n'est pas chargé.");
+            }
+        });
+    });
+});
