b38ada4932b2c52c3c0268b682d7f680063a5e4d
Apo <apolline.oresic@gmail.com>
2026-04-03 10:56:17 +0200

    Update translator-init.js

--- js/translator-init.js
+++ js/translator-init.js
@@ -1,16 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
+(function() {
+    var s = document.createElement("script");
+    s.src = "https://cdn.conveythis.com/javascript/conveythis.js";
+    s.onload = function() {
 
-    // boutons langues
-    document.querySelectorAll(".lang-btn").forEach(btn => {
-        btn.addEventListener("click", function() {
-          const lang = this.getAttribute("data-lang");
-
-           // Appel ConveyThis
-            if (typeof ConveyThis !== "undefined") {
-                ConveyThis.switchLanguage(lang);
-            } else {
-                console.error("ConveyThis n'est pas chargé.");
-            }
+        // Initialisation
+        ConveyThis_Init({
+            api_key: "pub_fb32793d8c366d7061fceaed2235e734"
         });
-    });
-});
+
+        // boutons de langues
+        document.querySelectorAll(".lang-btn").forEach(btn => {
+            btn.addEventListener("click", function() {
+                const lang = this.getAttribute("data-lang");
+                ConveyThis.switchLanguage(lang);
+            });
+        });
+    };
+    document.head.appendChild(s);
+})();
